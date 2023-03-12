e5442e56101a9151d6c83aef5b8f526703dd62cf
ilyakirpichev <ilyakirpichev@yandex-team.ru>
2023-03-13 02:14:55 +0300

    test

--- utils/feature_transformer_for_ml.py
+++ utils/feature_transformer_for_ml.py
@@ -6,7 +6,7 @@ from ml_models.input.bank_input_for_ml import BankInputForML
 
 class FeatureTransformerForMl:
     def __init__(self):
-        self.todo = 123
+        self.todo = 1234
 
     def transform(self, bank_with_features: FeatureCollectorBankOutput) -> BankInputForML:
         return BankInputForML(bank_with_features.latitude, bank_with_features.longitude)
