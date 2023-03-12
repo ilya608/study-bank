bdbbd214644d48baa1993577ce7198629a7a9174
ilyakirpichev <ilyakirpichev@yandex-team.ru>
2023-03-13 02:17:21 +0300

    test

--- ml_models/ml_manager.py
+++ ml_models/ml_manager.py
@@ -5,7 +5,7 @@ from ml_models.input.bank_input_for_ml import BankInputForML
 
 class MlManager:
     def __init__(self):
-        self.todo = 123
+        self.todo = 1234
 
     def predict(self, bank_with_features: Feature) -> float:
         # todo ML здесь
