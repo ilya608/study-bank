13aa6252fdf131db6945cf78f27375f07d848f14
ilya608 <ilyakirpichev@yandex.ru>
2023-03-15 12:26:12 +0300

    apply ML (#4)
    Co-authored-by: ilyakirpichev <ilyakirpichev@yandex-team.ru>

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import pickle
+
+import uvicorn
 from fastapi import FastAPI
 
 from features_collector.feature_collector_manager import FeatureCollectorManager
 from features_collector.input.feature_collector_bank_input import FeatureCollectorBankInput
-from ml_models.predictor import Predictor
+from ml.predictor import Predictor
 from utils.feature_transformer_for_ml import FeatureTransformerForMl
 
 app = FastAPI()
+
+with open("ml/models/atm_best.pkl", "rb") as f:
+    model = pickle.load(f)
+feature_collector_manager = FeatureCollectorManager()
+predictor = Predictor(model)
 
 
 @app.get("/abc")
@@ -15,14 +23,14 @@ async def root():
 
 @app.get("/predict-bank-quality")
 def predict(lat: float, long: float):
-    feature_collector_manager = FeatureCollectorManager()
-    feature_transformer_for_ml = FeatureTransformerForMl()
-    predictor = Predictor()
-
     feature_collector_bank_input = FeatureCollectorBankInput(latitude=lat, longitude=long)
     feature_collector_bank_output = feature_collector_manager.collect_features(feature_collector_bank_input)
 
-    bank_input_for_ml = feature_transformer_for_ml.transform(feature_collector_bank_output)
+    bank_input_for_ml = FeatureTransformerForMl.transform(feature_collector_bank_output)
     quality = predictor.predict(bank_input_for_ml)
 
-    return {"quality": quality}
+    return {"quality": quality[0]}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- features_collector/feature_collector_manager.py
+++ features_collector/feature_collector_manager.py
@@ -1,15 +1,17 @@
 from features_collector.input.feature_collector_bank_input import FeatureCollectorBankInput
 from features_collector.output.feature_collector_bank_output import FeatureCollectorBankOutput
+from features_collector.postgres.PostgresDao import PostgresDao
 
 
 class FeatureCollectorManager:
     def __init__(self):
-        self.todo = 123
+        self.postgres_dao = PostgresDao()
 
     def collect_features(self, feature_collector_bank_input: FeatureCollectorBankInput) -> FeatureCollectorBankOutput:
         lat = feature_collector_bank_input.latitude
         long = feature_collector_bank_input.longitude
 
-        # todo тут должна быть вся логика которая будет собирать фичи
+        # todo тут должна быть вся логика которая будет собирать фичи из бд, делать http запросы(если нужно итд)
+        # ответ она должна свой положить в FeatureCollectorBankOutput
 
         return FeatureCollectorBankOutput(latitude=lat, longitude=long)
