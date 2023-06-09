01bc9d94d641af6b2457b3959818f69d55f3ac74
ilyakirpichev <ilyakirpichev@yandex-team.ru>
2023-06-09 18:56:23 +0300

    sslrootcert

--- features_collector/postgres/postgres_connection.py
+++ features_collector/postgres/postgres_connection.py
@@ -10,7 +10,7 @@ def get_pg_connection():
         user=hse-ilya
         password=12345678
         target_session_attrs=read-write
-        sslrootcert=../../.postgresql/root.crt
+        sslrootcert=.postgresql/root.crt
     """)
 
     return conn
