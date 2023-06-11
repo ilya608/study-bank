28468e86e65e93750d3facbd96d45fc50149efc0
ilyakirpichev <ilyakirpichev@yandex-team.ru>
2023-06-12 01:32:33 +0300

    install

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ COPY . /app/
 WORKDIR /app
 # Установка зависимостей
 RUN pip install -r requirements.txt
+
+RUN mkdir --parents ~/.postgresql && wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" --output-document ~/.postgresql/root.crt && chmod 0600 ~/.postgresql/root.crt
--- features_collector/postgres/postgres_connection.py
+++ features_collector/postgres/postgres_connection.py
@@ -5,7 +5,7 @@ def get_pg_connection():
     conn = psycopg2.connect("""
         host=rc1a-mxoodqvw58cvt97d.mdb.yandexcloud.net,rc1b-4ny0b4t0wrstwjaj.mdb.yandexcloud.net
         port=6432
-        sslmode=verify-full
+        sslmode=require
         dbname=points
         user=hse-ilya
         password=12345678
@@ -19,12 +19,12 @@ def logs_pg_connection():
     conn = psycopg2.connect("""
         host=rc1a-mxoodqvw58cvt97d.mdb.yandexcloud.net,rc1b-4ny0b4t0wrstwjaj.mdb.yandexcloud.net
         port=6432
-        sslmode=verify-full
+        sslmode=require
         dbname=points
         user=hse-ilya
         password=12345678
         target_session_attrs=read-write
-        sslrootcert=../.postgresql/root.crt
+        sslrootcert=.postgresql/root.crt
     """)
 
     return conn
